d0be54f1ce493f562e265b21d5cbec606a38041f
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-01-13 01:07:48 +0530

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,15 @@ import { FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="bg-yellow-50 h-40 flex flex-col items-center justify-center">
-      {/* Logo */}
-      <img className="h-60 w-24 mb-4 object-cover" src={logo} alt="logo" />
-
-      {/* Icons */}
-      <div className="flex items-center space-x-4">
-        {/* Instagram */}
+    <div className="border shadow-gray-700 flex flex-col mt-5">
+      <img className="h-60 w-52 mb-4 object-cover" src={logo} alt="logo" />
+      <div className="flex space-x-4">
         <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
           <FaInstagram className="text-blue-500 hover:text-blue-700 cursor-pointer" />
         </a>
-
-        {/* Twitter */}
         <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
           <FaTwitter className="text-blue-500 hover:text-blue-700 cursor-pointer" />
         </a>
-
-        {/* LinkedIn */}
         <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin className="text-blue-500 hover:text-blue-700 cursor-pointer" />
         </a>
